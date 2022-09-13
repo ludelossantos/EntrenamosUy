449a5c230fe212fbf278c35dc6a060ab90e90144
ludelossantos <ludelossantosp@gmail.com>
2022-09-13 06:08:18 -0300

    finalizado

--- src/main/java/presentacion/AltaActividadDeportiva.java
+++ src/main/java/presentacion/AltaActividadDeportiva.java
@@ -1,5 +1,6 @@
 package presentacion;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,6 +18,7 @@ import javax.swing.JTextArea;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.BorderFactory;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 
@@ -80,6 +82,7 @@ public class AltaActividadDeportiva extends JInternalFrame {
 		textAreaDescripcion.setFont(new Font("Segoe UI", Font.PLAIN, 16));
 		textAreaDescripcion.setLineWrap(true);
 		textAreaDescripcion.setBounds(188, 107, 166, 81);
+		textAreaDescripcion.setBorder(BorderFactory.createLineBorder(Color.lightGray));
 		getContentPane().add(textAreaDescripcion);
 		
 		JLabel lblDuracion = new JLabel("Duraci\u00F3n");
