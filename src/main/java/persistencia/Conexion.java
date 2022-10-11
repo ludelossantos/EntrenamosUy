d71d1f55ebc1528bfa0225a4809d8de91e39e9e2
ludelossantos <ludelossantosp@gmail.com>
2022-10-11 18:13:13 -0300

    persistence y Conexion

--- src/main/java/persistencia/Conexion.java
+++ src/main/java/persistencia/Conexion.java
@@ -14,7 +14,7 @@ public class Conexion {
 	public static Conexion getInstancia() {
 		if (instancia == null) {
 			instancia = new Conexion();
-			emf = Persistence.createEntityManagerFactory("entreUY");
+			emf = Persistence.createEntityManagerFactory("entrenamosUy");
 			em=emf.createEntityManager();
 		}
 		return instancia;
